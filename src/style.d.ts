d73ae7e20dbb1edf70f358124be83619502e1056
Guan <821143943@qq.com>
2021-11-13 02:10:52 +0800

    Fix broken type definition

--- src/style.d.ts
+++ src/style.d.ts
@@ -1,6 +1,8 @@
 // this file fixes typing errors on using CSS variables in Vue template
 // https://github.com/vuejs/vue-next/issues/4741#issuecomment-936275184
 
+import { CSSProperties } from 'vue';
+
 declare module 'vue' {
   interface CSSProperties {
     [k: `--${string}`]: string;
